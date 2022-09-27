3a25b0f42f22d5e11176580a434f6335521a569d
Laziz Djuraev <110926594+laziz511@users.noreply.github.com>
2022-09-27 11:51:39 +0500

    Task is completed with some explanations !

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -11,12 +11,14 @@ public class FileReader {
         Profile profile = null;
 
         try (InputStream inputStream = new FileInputStream(file)) {
-
+            
+            // .split("\\R") = splits the String according to the beginning of the line, returns an array of split Strings.
+            // In this case it is divided into 4.
             String content = new String(inputStream.readAllBytes());
             String[] lines = content.split("\\R");
-
-            String name = lines[0].split(" ")[1];
-            Integer age = Integer.parseInt(lines[1].split(" ")[1]);
+            
+            String name = lines[0].split(" ")[1]; // split the line when " " is met, and return the second part.
+            Integer age = Integer.parseInt(lines[1].split(" ")[1]); // parseInt() = takes only a String as a parameter, and returns a primitive int value.
             String email = lines[2].split(" ")[1];
             Long phone = Long.parseLong(lines[3].split(" ")[1]);
 
